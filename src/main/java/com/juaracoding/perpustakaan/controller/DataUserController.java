beadd794986cdb549952489d2257329e356c6665
jouzu1 <jouzu1@gmail.com>
2021-05-30 02:22:11 +0700

    update controller

--- src/main/java/com/juaracoding/perpustakaan/controller/DataUserController.java
+++ src/main/java/com/juaracoding/perpustakaan/controller/DataUserController.java
@@ -1,10 +1,13 @@
 package com.juaracoding.perpustakaan.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -49,6 +53,12 @@ public class DataUserController {
 	@GetMapping("/name/{value}")
 	public DataUser getByName(@PathVariable("value") String value) {
 		return dataRepo.findByName(value);
+	}
+	
+	@GetMapping(value = "/image/{name}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public @ResponseBody byte[] getImageWithMediaType(@PathVariable String name) throws IOException {
+	   final InputStream in = getClass().getResourceAsStream("/user-photo/"+name);
+	   return IOUtils.toByteArray(in);
 	}
 	
 	@PostMapping("/register")
